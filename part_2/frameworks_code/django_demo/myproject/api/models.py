ab59ccd76dc4306b352b08bb8ef5222f101bfc3c
Lautaro <lautaorsi@gmail.com>
2026-03-12 17:13:54 -0300

    Fixed Django serializer

--- part_2/frameworks_code/django_demo/myproject/api/models.py
+++ part_2/frameworks_code/django_demo/myproject/api/models.py
@@ -8,6 +8,7 @@
 from django.db import models
 
 
+# Modelo para curso
 class Courses(models.Model):
     course_id = models.AutoField(primary_key=True)
     course_title = models.CharField(max_length=255)
--- part_2/frameworks_code/django_demo/myproject/api/serializers.py
+++ part_2/frameworks_code/django_demo/myproject/api/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 from .models import Courses
 
+# Serializador de curso
 class CourseSerializer(serializers.ModelSerializer):
         class Meta:
                     model = Courses
-                    fields = ['course_id', 'subject_code', 'course_title', 'description', 'credits', 'professor_name', 'semester', 'capacity', 'created_at' ]
+                    fields = ['course_id', 'course_title', 'credits']
 
 
