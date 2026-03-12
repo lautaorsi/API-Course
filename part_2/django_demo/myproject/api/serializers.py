4b6e1822521f0ad1f0951f4ff38ed3b4a78939a5
Lautaro <lautaorsi@gmail.com>
2026-03-12 00:00:01 -0300

    2 parts TODO, django part worked on

--- part_2/django_demo/myproject/api/serializers.py
+++ part_2/django_demo/myproject/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Item
 
-class ItemSerializer(serializers.ModelSerializers):
-        class meta:
-                    model = Itemfields = ['id','name','description']
+class ItemSerializer(serializers.ModelSerializer):
+        class Meta:
+                    model = Item
+                    fields = ['id','name','description']
